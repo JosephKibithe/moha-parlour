a20281817ce37cd1c7790aa9b432a5fd97b5c1fb
ZenithAug <271772990+ZenithAug@users.noreply.github.com>
2026-07-07 16:37:31 +0300

    chore: configure webpack for dev, rename proxy to middleware, and setup OpenNext Cloudflare

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "moha-parlour.pages.dev",
+        "*.moha-parlour.pages.dev",
+        "moha-parlour.joethetraplord.workers.dev",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
